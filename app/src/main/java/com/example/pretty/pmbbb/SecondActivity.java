eb9e6dbe978be45c33dc07ac19b843e7e2ec8035
dilectorpe <dilector@naver.com>
2018-09-08 22:11:21 +0900

    first upload

---
+++ app/src/main/java/com/example/pretty/pmbbb/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.pretty.pmbbb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+    public void onBackButtonClick(View v){ finish();}
+}
